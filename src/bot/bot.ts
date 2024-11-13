2d952ab387e2876211ca3894523cbbd3bb9a79f0
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2024-11-13 10:36:18 +0500

    fix filter middlware

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -7,11 +7,7 @@ import privateCommands from "../command/private/index.js";
 import groupCommands from "../command/group/index.js";
 import { CommandType } from "../command/type.js";
 import echo from "../middleware/echo.js";
-import {
-  allowGroup,
-  groupChatFilter,
-  privateChatFilter
-} from "../middleware/filter.js";
+import { groupChatFilter, privateChatFilter } from "../middleware/filter.js";
 
 const bot = new Bot<MyContext>(config.botToken);
 
@@ -19,7 +15,7 @@ const privateChat = privateChatFilter(bot);
 const groupChat = groupChatFilter(bot);
 
 privateChat.use(privateChatComposer);
-groupChat.use(allowGroup, groupChatComposer);
+groupChat.use(groupChatComposer);
 
 bot.api.setMyCommands(privateCommands, {
   scope: { type: CommandType.private }
--- src/middleware/filter.ts
+++ src/middleware/filter.ts
@@ -1,15 +1,9 @@
-import { NextFunction } from "grammy";
-import config from "../config/config.js";
 import type MyBot from "../types/bot";
-import type MyContext from "../types/context";
 
-export const privateChatFilter = (bot: MyBot) =>
-  bot.filter((ctx) => ctx.chat?.type === "private");
+export const privateChatFilter = (bot: MyBot) => {
+  return bot.filter((ctx) => ctx.chat?.type === "private");
+};
 
-export const groupChatFilter = (bot: MyBot) =>
-  bot.filter((ctx) => ctx.chat?.type === "supergroup");
-
-export const allowGroup = (ctx: MyContext, next: NextFunction) => {
-  if (ctx.update.message!.chat.id === config.groupId) return next();
-  return `https://t.me/${ctx.update.message!.chat} group not allowed`;
+export const groupChatFilter = (bot: MyBot) => {
+  return bot.filter((ctx) => ctx.chat?.type === "supergroup");
 };
